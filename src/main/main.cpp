5934af0cd4b40440528fd1aa5d1d5a718db2b124
Karim DRIDI <karim.dridi@gmail.com>
2019-01-29 19:25:32 +0100

    clean project

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,16 +1,17 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::Window window(sf::VideoMode(80 * 16, 45 * 16), "vimGL");
+#define SCALE 16
+#define WIDTH (80 * SCALE)
+#define HEIGHT (45 * SCALE)
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "vimGL");
     
     // run the program as long as the window is open
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
@@ -25,8 +26,10 @@ int main()
                         break;
                 }
         }
+        
+        window.clear(sf::Color::Black);
+        window.display();
     }
     
     return 0;
 }
-
